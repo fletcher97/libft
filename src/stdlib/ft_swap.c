8a68c6b66dd52407885058edeeee3b866e55bad9
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-02-14 23:07:08 +0000

    Fixed missing include

--- src/stdlib/ft_swap.c
+++ src/stdlib/ft_swap.c
@@ -6,11 +6,12 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 13:06:41 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/02/13 14:22:23 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/02/13 22:10:55 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdlib.h"
+#include "ft_string.h"
 
 void	ft_swap(void *e1, void *e2, size_t size)
 {
