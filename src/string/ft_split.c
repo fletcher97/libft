ae80bf5016c0c126a6e800d0754f71f66fc864ee
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-02-14 22:41:28 +0000

    Removed old static function calls

--- src/string/ft_split.c
+++ src/string/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 01:22:42 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/02/13 20:30:59 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/02/13 22:48:32 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	delim[0] = c;
 	delim[1] = '\0';
-	cnt = ft_strwcnt_s(s, delim) + 1;
+	cnt = ft_strwcnt(s, delim) + 1;
 	if (!ft_set64((int64_t *)&ret, (int64_t)ft_malloc(sizeof(char *) * (cnt))))
 		return (NULL);
 	ret[--cnt] = NULL;
@@ -34,9 +34,9 @@ char	**ft_split(char const *s, char c)
 		return (ret);
 	dup = ft_strdup(s);
 	pos = 0;
-	ret[pos] = ft_strdup(strtok_s(dup, delim));
+	ret[pos] = ft_strdup(ft_strtok(dup, delim));
 	while (++pos < cnt)
-		ret[pos] = ft_strdup(strtok_s(NULL, delim));
+		ret[pos] = ft_strdup(ft_strtok(NULL, delim));
 	ft_free(dup);
 	return (ret);
 }
