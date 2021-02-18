6cfea6176cd6076b882ce4bf2b44be329a806d14
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-02-18 09:53:05 +0000

    Added long base conversions

--- inc/ft_conv.h
+++ inc/ft_conv.h
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 15:58:16 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/02/18 08:54:42 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/02/18 09:46:57 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 char	*ft_itoa_base(int n, const char *base);
 char	*ft_itoa(int n);
+char	*ft_ltoa_base(long l, const char *base);
 char	*ft_ltoa(long l);
 char	*ft_uitoa(unsigned int n);
+char	*ft_ultoa_base(unsigned long l, const char *base);
 char	*ft_ultoa(unsigned long l);
 
 #endif
