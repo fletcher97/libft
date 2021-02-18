bc944a68917ce768fb411a464725983845569d5d
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-02-18 20:38:59 +0000

    Defined standard fds

--- inc/ft_stdio.h
+++ inc/ft_stdio.h
@@ -6,12 +6,16 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:15:07 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/02/13 20:21:50 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/02/18 20:37:59 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STDIO_H
 # define FT_STDIO_H
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
