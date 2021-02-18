9629a21b647acc530139e2d8f7e3daff2f9a5322
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-02-18 10:29:11 +0000

    Added uitoa_base as well as HEX define for upper and lower case

--- inc/ft_conv.h
+++ inc/ft_conv.h
@@ -6,17 +6,21 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 15:58:16 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/02/18 09:46:57 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/02/18 10:27:26 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_CONV_H
 # define FT_CONV_H
 
+# define HEX_L "123456789abcdef"
+# define HEX_U "123456789ABCDEF"
+
 char	*ft_itoa_base(int n, const char *base);
 char	*ft_itoa(int n);
 char	*ft_ltoa_base(long l, const char *base);
 char	*ft_ltoa(long l);
+char	*ft_uitoa_base(unsigned int n, const char *base);
 char	*ft_uitoa(unsigned int n);
 char	*ft_ultoa_base(unsigned long l, const char *base);
 char	*ft_ultoa(unsigned long l);
