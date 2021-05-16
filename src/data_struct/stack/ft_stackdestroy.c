5a4421369bff0a27245de1f6a210c4e1ec0740f6
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-05-16 02:36:29 +0100

    Norm 3.2.95 quick fix

--- src/data_struct/stack/ft_stackdestroy.c
+++ src/data_struct/stack/ft_stackdestroy.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 01:51:24 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/05/16 01:55:12 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/05/16 02:35:46 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 void	ft_stackdestroy(t_stack *stack, void (*del)(void *))
 {
 	while (ft_stacksize(stack))
+	{
 		if (del)
 			del(ft_stackpop(stack));
 		else
 			ft_stackpop(stack);
+	}
 	ft_free(stack);
 }
