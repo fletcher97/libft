65d0c48a403ca025e7e2aa4b70c3e89108aae786
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-05-30 03:18:18 +0100

    Added dup and print functions to the stack structure

---
+++ src/data_struct/stack/ft_stackprint.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stackprint.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/21 19:18:50 by mgueifao          #+#    #+#             */
+/*   Updated: 2021/05/30 03:14:22 by mgueifao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stack.h"
+#include "ft_stdio.h"
+#include "ft_conv.h"
+
+void ft_stackprint(t_stack *s, char *(*toString)(void *), char* delim)
+{
+	t_stack_elem *ele;
+
+	if (!s)
+		return;
+	ele = s->top;
+	ft_putstr_fd(ft_ultoa_base(ele->content, HEX_U), 1);
+	while (ele)
+	{
+		ft_putstr_fd(delim, 1);
+		ft_putstr_fd(ft_ultoa_base(ele->content, HEX_U), 1);
+	}
+	ft_putstr_fd("\n", 1);
+}
