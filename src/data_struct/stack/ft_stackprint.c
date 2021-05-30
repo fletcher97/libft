93dcf50020c64a8b2bf047678d86d0ca587c84c0
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-05-30 07:33:34 +0100

    Fixed printing one extra delim at the begining

--- src/data_struct/stack/ft_stackprint.c
+++ src/data_struct/stack/ft_stackprint.c
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 19:18:50 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/05/30 07:30:01 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/05/30 07:33:16 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,18 @@ void ft_stackprint(t_stack *s, char *(*toString)(void *), char* delim)
 	if (!s)
 		return;
 	ele = s->top;
+	if (toString && ele)
+		ft_putstr_fd(toString(ele->content), 1);
+	else if (ele)
+		ft_putstr_fd(ft_ultoa_base((unsigned long)ele->content, HEX_U), 1);
 	while (ele)
 	{
+		ele = ele->prev;
 		ft_putstr_fd(delim, 1);
 		if (toString)
 			ft_putstr_fd(toString(ele->content), 1);
 		else
 			ft_putstr_fd(ft_ultoa_base((unsigned long)ele->content, HEX_U), 1);
-		ele = ele->prev;
 	}
 	ft_putstr_fd("\n", 1);
 }
