b362dcfc8cfc452d2ba24a3bc3d38bcf7c5f6ea4
Miguel Gueifao <miguel.gueifao@gmail.com>
2021-05-30 07:05:56 +0100

    Added print and dup to .h

--- inc/ft_stack.h
+++ inc/ft_stack.h
@@ -6,7 +6,7 @@
 /*   By: mgueifao <mgueifao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/16 00:46:07 by mgueifao          #+#    #+#             */
-/*   Updated: 2021/05/16 01:55:52 by mgueifao         ###   ########.fr       */
+/*   Updated: 2021/05/30 07:05:19 by mgueifao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,11 @@ typedef struct s_stack
 }				t_stack;
 
 void	ft_stackdestroy(t_stack *stack, void (*del)(void *));
+t_stack *ft_stackdup(t_stack *o, void *(*dup)(void *));
 t_stack	*ft_stacknew(void);
 void	*ft_stackpeek(t_stack *stack);
 void	*ft_stackpop(t_stack *stack);
+void	ft_stackprint(t_stack *s, char *(*toString)(void *), char* delim);
 void	ft_stackpush(t_stack *stack, void *content);
 size_t	ft_stacksize(t_stack *stack);
 
